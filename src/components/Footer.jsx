dc020a141e4f35df9b523bbf178413fd9bee8d97
Bharat Paliwal <18uec169@lnmiit.ac.in>
2021-06-20 21:22:48 +0530

    security updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
-import {Link} from 'react-router-dom';
 function Footer(props) {
   return (
     <React.Fragment>
       <div className="container-fluid">
         <div className="row bg-dark text-white">
-          <div className="col-6 mt-5 text-center">
+          {/* <div className="col-6 mt-5 text-center">
             <h6>Liked the Feed!!</h6>
-            <p>Appriciate the developer by giving Star <span> <Link rel="noopener" to='https://github.com/bharatpaliwal-169/NewsFeed' target='_blank'>Here</Link> </span></p>
           </div>
           <div className="col-6 mt-5 text-center">
             <h6>Developer's Profile :</h6>
             <p>
-              <Link to="https://github.com/bharatpaliwal-169/" target="_blank" rel="noopener">Here</Link>
             </p>
-          </div>
+          </div> */}
           <div className="col-12 text-center">
             <p className='text-muted'>&copy; Bharat Paliwal</p>
           </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import {Navbar,Nav} from 'react-bootstrap';
+import {Navbar} from 'react-bootstrap';
 export default function Header(props) {
   return (
     <React.Fragment>
       <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark" fixed='top'>
         <Navbar.Brand className="ms-5">What's the News ? </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             
           </Nav>
           <Nav>
-            <a className="nav-link active"  href="https://github.com/bharatpaliwal-169/NewsFeed" rel="noreferrer" target="_blank">
+            {/* <a className="nav-link active"  href="https://github.com/bharatpaliwal-169/NewsFeed" rel="noreferrer" target="_blank">
               <Nav.Item className="ms-5 me-5">
                   Star &#38; Fork on GitHub
               </Nav.Item>
-            </a>
+            </a> *
           </Nav>
-        </Navbar.Collapse>
+        </Navbar.Collapse> */}
       </Navbar>
     </React.Fragment>
   )
